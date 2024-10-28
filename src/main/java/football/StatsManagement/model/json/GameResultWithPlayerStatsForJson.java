d923fb81710a0c8e5f11a879c8893db187a56d69
Aiji Yamamura <j2yamimjr3i@gmail.com>
2024-10-28 19:15:03 +0900

    レコードクラスからGetter削除

--- src/main/java/football/StatsManagement/model/json/GameResultWithPlayerStatsForJson.java
+++ src/main/java/football/StatsManagement/model/json/GameResultWithPlayerStatsForJson.java
@@ -3,11 +3,8 @@ package football.StatsManagement.model.json;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 @Schema(description = "試合結果と選手試合成績一覧を登録するための情報を保持するクラス")
-@Getter // 修正を避けるためGetterは残す
 public record GameResultWithPlayerStatsForJson(
     @Valid GameResultForJson gameResultForJson,
     @Valid List<PlayerGameStatForJson> homeClubPlayerGameStatsForJson,
