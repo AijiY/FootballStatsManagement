448bdb3936993f4b52bc33498a75d2b3d66ec40b
Aiji Yamamura <j2yamimjr3i@gmail.com>
2024-11-01 20:36:31 +0900

    WebconfigをS3用に修正

--- src/main/java/football/StatsManagement/configuration/WebConfig.java
+++ src/main/java/football/StatsManagement/configuration/WebConfig.java
@@ -1,5 +1,6 @@
 package football.StatsManagement.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,19 +11,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//  @Value("${REACT_APP_URL}")
+  @Value("${REACT_APP_URL}")
   private String reactAppUrl;
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
 
-    reactAppUrl = "http://localhost:3000";
+//    reactAppUrl = "http://localhost:3000";
 
     registry.addMapping("/**")
       .allowedOrigins(reactAppUrl)
-      .allowedMethods("GET", "POST", "PUT", "DELETE");
-//      .allowedHeaders("*")
-//      .allowCredentials(true); // Cookieなどの送信を許可
+      .allowedMethods("GET", "POST", "PUT", "DELETE")
+      .allowedHeaders("*")
+      .allowCredentials(true); // Cookieなどの送信を許可
   }
 
 }
