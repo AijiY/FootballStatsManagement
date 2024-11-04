fa9f03f6f63839d52046039311180a4512803581
Aiji Yamamura <j2yamimjr3i@gmail.com>
2024-11-04 16:06:39 +0900

    機能追加：deploy.ymlにswaggerからapi-docsの生成タスク追加

--- src/main/java/football/StatsManagement/Application.java
+++ src/main/java/football/StatsManagement/Application.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(info = @Info(title = "サッカースタッツ管理システム"))
+@OpenAPIDefinition(info = @Info(title = "サッカースタッツ管理システム", description = "サッカーのスタッツを管理するためのAPI"))
 @SpringBootApplication
 public class Application {
 
