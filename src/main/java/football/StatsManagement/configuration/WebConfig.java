ee9ca75bddafa21d28ac4627459886227253381e
Aiji Yamamura <j2yamimjr3i@gmail.com>
2024-11-04 15:12:50 +0900

    application.propertiesにreact.app.urlを設定

--- src/main/java/football/StatsManagement/configuration/WebConfig.java
+++ src/main/java/football/StatsManagement/configuration/WebConfig.java
@@ -11,13 +11,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//  @Value("${REACT_APP_URL:http://localhost:3000}") // テスト用にデフォルト値を設定
+  @Value("${react.app.url}")
   private String reactAppUrl;
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-
-    reactAppUrl = "http://my-client-page-bucket.s3-website-ap-northeast-1.amazonaws.com";
 
     registry.addMapping("/**")
       .allowedOrigins(reactAppUrl)
